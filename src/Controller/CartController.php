2bb2c03219c62c44553fc0bf9c4d8ad0a1654f00
neboysam <monte.nemi@gmail.com>
2022-05-05 16:16:13 +0200

    added cart view

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,11 +13,23 @@ class CartController extends AbstractController
     /**
      * @Route("/mon-panier", name="cart")
      */
-    public function index(Cart $cart): Response
+    public function index(ProductRepository $repository, Cart $cart): Response
     {
         //dd($cart->get()); //gives > array:1 [▼ 1 => 1] i.e. id => quantity
-        
-        return $this->render('cart/cart.html.twig');
+        $completeCart = [];
+        if ($cart->get()) {
+            foreach ($cart->get() as $id => $quantity) {
+                $completeCart[] = [
+                    'quantity' => $quantity,
+                    /* 'products' => $this->entityManager->getRepository(Product::class)->findById($id) */
+                    'product' => $repository->findOneById($id)
+                ];
+            }
+        }
+
+        return $this->render('cart/cart.html.twig', [
+            'cart' => $completeCart
+        ]);
     }
 
     /**
